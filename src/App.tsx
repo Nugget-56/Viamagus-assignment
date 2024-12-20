065d79372d089524dcde36f7ed2c062b5164c6e4
Aayush <aayushsingh610@gmail.com>
2024-12-20 15:53:02 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './globals.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
