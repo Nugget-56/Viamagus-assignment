d2141de90b69d51a05991af06a56ff6a9197698a
Aayush <aayushsingh610@gmail.com>
2024-12-22 21:38:18 +0530

    minor UI change

--- src/components/task-2/PostCard.tsx
+++ src/components/task-2/PostCard.tsx
@@ -2,8 +2,8 @@ import { Post } from "./PostsFeed"
 
 export function PostCard({ post }: { post: Post }) {
   return (
-    <div className="flex flex-col justify-between p-4 bg-white text-black rounded-md border border-slate-300 w-full min-h-48 overflow-hidden">
-      <div className="font-bold text-xl mb-2">
+    <div className="flex flex-col gap-2 p-4 bg-white text-black rounded-md border border-slate-300 w-full min-h-48 overflow-hidden">
+      <div className="font-bold text-xl mb-2 leading-6">
         {post.title}
       </div>
       <div className="text-base">
